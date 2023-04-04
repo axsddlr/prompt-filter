55a857834517c546fe3d6e09f78e17336a436fdc
Andre Saddler <dresadd09691@gmail.com>
2023-04-04 01:35:10 -0400

    refactor(InputBox.js): improve input parsing to handle nested parentheses and brackets

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const InputBox = ({ onTokensGenerated }) => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
 
   const handleSubmit = () => {
-    const tokens = input.split(',').map(token => token.trim());
+    const tokens = input
+      .split(/,(?![^\[\]\(\)]*[\]\)])/)
+      .map((token) => token.trim());
     onTokensGenerated(tokens);
   };
 
   return (
     <div className="flex items-center">
-      <label htmlFor="inputBox" className="block text-gray-700 dark:text-gray-300 text-sm font-bold mr-2">Input:</label>
+      <label
+        htmlFor="inputBox"
+        className="block text-gray-700 dark:text-gray-300 text-sm font-bold mr-2"
+      >
+        Input:
+      </label>
       <input
         className="flex-grow p-2 bg-gray-200 dark:bg-gray-800 dark:text-white rounded-l-md focus:outline-none focus:ring-2 focus:ring-indigo-400"
         type="text"
@@ -18,7 +25,10 @@ const InputBox = ({ onTokensGenerated }) => {
         value={input}
         onChange={(e) => setInput(e.target.value)}
       />
-      <button className="bg-indigo-500 text-white p-2 rounded-r-md focus:outline-none hover:bg-indigo-600" onClick={handleSubmit}>
+      <button
+        className="bg-indigo-500 text-white p-2 rounded-r-md focus:outline-none hover:bg-indigo-600"
+        onClick={handleSubmit}
+      >
         Submit
       </button>
     </div>
